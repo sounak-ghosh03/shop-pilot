37a14a3833eb9173098bf82577cde88dd2a9e107
SOUNAK GHOSH <sounak2309@gmail.com>
2025-08-28 13:22:37 +0530

    added files

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,6 @@ app.use("/api/order", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server is running on port ${PORT}`);
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
 });
